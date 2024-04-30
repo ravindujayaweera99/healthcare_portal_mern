38a5022576da962438cdbc2500de602c0b28c53e
KChathuranga <kavinduchathuranga12@gmail.com>
2024-04-30 14:25:12 +0530

    Make the path for Patient login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminLogin from "./Components/Pages/Admin/AdminLogin/AdminLogin";
+import PatientLogin from "./Components/Pages/Patient/PatientLogin/PatientLogin";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/adminlogin" element={<AdminLogin />} />
+          <Route path="/patientLogin" element={<PatientLogin/>} />
         </Routes>
       </BrowserRouter>
     </div>
