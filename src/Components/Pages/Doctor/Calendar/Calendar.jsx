e51022526b4fcab514b4e7daed7b12d22bcd4187
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2024-05-14 13:44:34 +0530

    Create Calendar component

---
+++ src/Components/Pages/Doctor/Calendar/Calendar.jsx
@@ -0,0 +1,19 @@
+// import React, { useState, useEffect } from 'react';
+import './Calendar.css'
+
+const Calendar = () => {
+  
+    return (
+      <div className="Calendar">
+        <div className="CalendarHeader">
+          <h2>Calendar</h2>
+        </div>
+        <div className="CalendarContent">
+
+        </div>
+      </div>
+    );
+  };
+  
+  export default Calendar;
+  
