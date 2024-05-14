c7375bcdd38ec8919992bdb2c28330d5960e4c17
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2024-05-14 13:46:20 +0530

    Create Prescription component

---
+++ src/Components/Pages/Doctor/Prescriptions/Prescription.jsx
@@ -0,0 +1,19 @@
+// import React, { useState, useEffect } from 'react';
+import './Prescription.css';
+
+const Prescription = () => {
+
+    return (
+      <div className="Prescription">
+        <div className="PrescriptionHeader">
+          <h2>Prescriptions</h2>
+        </div>
+        <div className="PrescriptionContent">
+
+        </div>
+      </div>
+    );
+  };
+  
+  export default Prescription;
+  
