64d725eb53eb562bb7e7f1141a5cf3d6cddb27e0
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2024-05-14 13:46:39 +0530

    Create doctor settings component

---
+++ src/Components/Pages/Doctor/Settings/Settings.jsx
@@ -0,0 +1,19 @@
+// import React, { useState, useEffect } from 'react';
+import './Settings.css';
+
+const Settings = () => {
+  
+    return (
+      <div className="Settings">
+        <div className="SettingsHeader">
+          <h2>Settings</h2>
+        </div>
+        <div className="SettingsContent">
+
+        </div>
+      </div>
+    );
+  };
+  
+  export default Settings;
+  
