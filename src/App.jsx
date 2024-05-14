dcd48597f7e04b1e6c7381d14f482f157a7597ee
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2024-05-14 13:42:35 +0530

    Create doctor login route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminLogin from "./Components/Pages/Admin/AdminLogin/AdminLogin";
 import PatientLogin from "./Components/Pages/Patient/PatientLogin/PatientLogin";
+import DoctorLogin from "./Components/Pages/Doctor/DoctorLogin/DoctorLogin";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/adminlogin" element={<AdminLogin />} />
           <Route path="/patientlogin" element={<PatientLogin/>} />
+          <Route path="/doctorlogin" element={<DoctorLogin/>} />
         </Routes>
       </BrowserRouter>
     </div>
