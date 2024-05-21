d45d691dc3aaa7457ae32e024ec3fe53bcdf579d
Ravindu Jayaweera <ravindujayaweera99@gmail.com>
2024-05-21 23:26:45 +0530

    Dashboard Sidebar completed with state updating

---
+++ src/Components/Pages/Admin/PatientDetails/PatientDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PatientDetails = () => {
+  return (
+    <div>
+      <h1>Patient Details</h1>
+    </div>
+  );
+};
+
+export default PatientDetails;
---
+++ src/Components/Pages/Admin/TransactionDetails/TransactionDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransactionDetails = () => {
+  return (
+    <div>
+      <h1>Transaction Details</h1>
+    </div>
+  )
+}
+
+export default TransactionDetails
